6d42852e2043139b736596f74109c6b7a1c278ca
Amber Lyons <amber.lyons38@gmail.com>
2023-05-03 18:16:48 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,6 @@ function IndexPage() {
   return (
     <div className="App">
       <Palette />
-      <div className="App-Icon">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from
-        <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
     </div>
   );
 }
