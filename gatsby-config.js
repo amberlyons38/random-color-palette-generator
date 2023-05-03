8b04a188bf545631ea49c43385e963c8c41939ca
Amber Lyons <amber.lyons38@gmail.com>
2023-05-03 16:59:44 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+  pathPrefix: "/random-color-palette-generator",
   plugins: [
     `gatsby-plugin-image`,
     {
